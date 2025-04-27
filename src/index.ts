25c14fce4dbd8a49efa3194d61107d38d4aacb68
Chelsea Caines <chelseajcaines@hotmail.com>
2025-04-27 10:20:10 -0230

    added build.config

--- src/index.ts
+++ src/index.ts
@@ -48,22 +48,6 @@ app.use('/api/savings', savingsRouter)
 app.use('/api/auth', forgotPasswordRouter); // Mounts the forgot password routes at /api/auth/forgot-password
 app.use('/api/auth', resetPasswordRouter); // Mounts the reset password routes at /api/auth/reset-password
 
-/**
- * Exercise:
- * Implement a new data model and corresponding API. At the end of the day you should
- * create a model that interests you, and progresses your project in some way, but if
- * you can't think of anything, try creating a model for a collection of books which
- * may have the following properties:
- *  - ISBN
- *  - title
- *  - author
- *  - etc...
- * and lives at /api/book
- *
- * Whatever you decide be sure to implement the full set of POST, GET, PUT, and DELETE
- * operations assuming that makes sense in your case.
- */
-
 app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
 // starts the server and makes it listen on the specified port (PORT). When the server starts, it logs a 
 // message to the console indicating that it is running and the port number it is listening on
